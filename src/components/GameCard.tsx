9c01baac45a842b461a30d1ac09c0b5afbdc8592
Your Name <18314455297@163.com>
2025-02-07 15:09:41 +0800

    display platform icon

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,22 @@
+
+import { Game } from '../hooks/useGames'
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
+import PlatformIconList from './PlatformIconList'
+
+interface Pros{
+    game: Game
+}
+
+const GameCard = ({ game }:Pros) => {
+  return (
+    <Card borderEndRadius={10} overflow='hidden'>
+        <Image src={game.background_image} />
+        <CardBody>
+            <Heading fontSize={'2xl'}>{game.name}</Heading>
+            <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)} />
+        </CardBody>
+    </Card>
+  )
+}
+
+export default GameCard
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,18 @@ import React, { useEffect, useState } from "react";
 import apiClient from "../service/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Platform {
   id: number;
   name: string;
+  slug: string;
+}
+
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms:{ platform:Platform }[]
 }
 
 interface FetchGamesResponse {
