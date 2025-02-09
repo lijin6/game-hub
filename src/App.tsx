a40b25c5c3093de789ca01617ac96bd97b370c84
Your Name <18314455297@163.com>
2025-02-09 20:23:31 +0800

    build selectplatform

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Grid, GridItem, Show} from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 const App = () => {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null >(null)
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   return (
     <Grid
       templateAreas={{
@@ -14,8 +15,8 @@ const App = () => {
         lg: `"nav nav" "aside main"`, // 大屏幕：nav 在上，aside 和 main 横向排布
       }}
       templateColumns={{
-        base:'1rf',
-        lg:'200px',
+        base: "1rf",
+        lg: "200px",
       }}
     >
       <GridItem area="nav">
@@ -25,13 +26,15 @@ const App = () => {
       {/* 在大屏幕显示 aside，且小屏幕隐藏 */}
       <Show above="lg">
         <GridItem area="aside" bg="" paddingX={"5"}>
-          <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main" bg="">
-
-        <GameGrid selectedGenre={selectedGenre}/>
-        
+        <PlatformSelector />
+        <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
