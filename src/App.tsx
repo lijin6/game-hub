3ec3d65cbf7851600f2d81d39cd03b9d6a3f44f8
Your Name <18314455297@163.com>
2025-02-09 21:23:37 +0800

    sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 const App = () => {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
@@ -45,8 +46,8 @@ const App = () => {
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
             onSelectPlatform={(platform) => setGameQuery({...gameQuery,platform})}
-          />
-          <SortSelector />
+          /> 
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=> setGameQuery({...gameQuery,sortOrder})} />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
