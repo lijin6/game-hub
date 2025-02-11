ceea20678e69ebce29da3a34dba23c98ec704f18
Your Name <18314455297@163.com>
2025-02-11 21:39:30 +0800

    customize the  theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,28 @@
 import { ThemeConfig, extendTheme } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 const config: ThemeConfig = {
-    initialColorMode: 'dark'
+  initialColorMode: "dark",
 };
 
-const theme = extendTheme({config});
+const theme = extendTheme({
+     config,
+     color:{
+        gray:{
+            50:'#f9f9f9',
+            100:'#f3f3f3',
+            200:'#e5e5e5',
+            300:'#d4d4d4',
+            400:'#a3a3a3',
+            500:'#73737' ,
+            600:'#525252',
+            700 : '#404040',
+            800:'#262626',
+            900:'#171717'
+        }
+     }
+    
+    
+    });
 
 export default theme;
