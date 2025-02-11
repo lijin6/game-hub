da7b9e478a39f46459a42312f652597b0c5fa55e
Your Name <18314455297@163.com>
2025-02-11 21:12:09 +0800

    Add a dynamic page heading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,31 +6,32 @@ import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
-interface Props{
+interface Props {
   gameQuery: GameQuery;
 }
 
-const GameGrid = ({gameQuery}: Props) => {
-
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 4}} spacing={3} padding={'10px'}>
-        
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 4 }}
+        spacing={6}
+        padding={"10px"}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer  key={skeleton}>
+            <GameCardContainer key={skeleton}>
               <GameCardSkelenton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer key={game.id} >
+          <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
-
         ))}
       </SimpleGrid>
     </>
