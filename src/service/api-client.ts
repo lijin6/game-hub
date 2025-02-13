60d46d37f49063c6fdb6f544e1a18e88e49580a5
TodoPasa <113897036+KingLiu54@users.noreply.github.com>
2025-02-13 16:13:22 +0800

    Update api-client.ts
    update api-client method

--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '089203aa3afa4a0d8f351e9208b028b7',
+        key: 'input your self raeg.io key ',
     }
 })
